06bd2c6d70be7bc6af4de85d8bc6926f5ebba9c3
Jose C. Massón <jose@gcoop.coop>
2013-01-24 20:56:12 -0300

    Solve circula imports :D

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,24 +1,5 @@
-class Command:
-
-    def install(self, console):
-        self.console = console
-        command = self.__class__.__name__
-        methods = ['do', 'help', 'complete']
-
-        for m in methods:
-            # referencia al metodo
-            handler = getattr(self, m)
-            setattr(console, "%s_%s" %(m, command.lower()), handler)
-            setattr(console, "%s_%s" %(m, command.upper()), handler)
-
-    def do(self, stm):
-        pass
-
-    def help(self):
-        pass
-
-    def complete(self, text, line, begidx, endidx):
-        pass
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from select import Select
 from insert import Insert
