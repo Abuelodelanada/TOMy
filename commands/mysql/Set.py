d952a32413231b9a2d8ae3a642f4178f0378e548
Jose C. Massón <jose@gcoop.coop>
2013-03-21 12:40:37 -0500

    Refactor: get_autocommit() method created

--- commands/mysql/Set.py
+++ commands/mysql/Set.py
@@ -7,8 +7,8 @@ import commands
 class Set(commands.Command):
     def do(self, stm):
         self.console.default('SET %s' % stm)
-        ac = self.console.raw_query("SHOW variables LIKE 'autocommit'")
-        self.console.connection_data['autocommit'] = ac[0][1]
+
+        self.console.connection_data['autocommit'] = self.get_autocommit()
         self.console.prompt = self.console.get_prompt(
                     self.console.connection_data['user'],
                     self.console.connection_data['host'],
@@ -25,3 +25,9 @@ class Set(commands.Command):
         | [@@global. | @@session. | @@]system_var_name = expr
         """
         print help
+
+    def get_autocommit(self):
+        """
+        """
+        ac = self.console.raw_query("SHOW variables LIKE 'autocommit'")
+        return ac[0][1]
